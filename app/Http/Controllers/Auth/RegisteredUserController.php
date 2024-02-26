5fd7401fa0a95d0d1c05822c8a25b345ae27a476
yassirelkhaili <elkhailiyassir@gmail.com>
2024-02-26 16:26:20 +0100

    commit

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,7 +35,6 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-
         return response()->noContent();
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
